1c10b967ba6a0738458a7bd94694e736a5a8c0a1
chinocarbon <1070980757@qq.com>
2024-01-03 17:35:36 +0800

    使用类加载器加载资源文件，简化了操作

--- src/main/java/org/chinocarbon/judgesystem/listener/MyServletContextListener.java
+++ src/main/java/org/chinocarbon/judgesystem/listener/MyServletContextListener.java
@@ -7,7 +7,10 @@ package org.chinocarbon.judgesystem.listener;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 import java.util.ResourceBundle;
 
@@ -23,27 +26,22 @@ public class MyServletContextListener implements ServletContextListener, HttpSes
     public void contextInitialized(ServletContextEvent sce)
     {
         Properties properties = new Properties();
-        try
-        {
-            properties.load(new FileInputStream("C:\\Users\\12992\\IdeaProjects\\HebeuOJ-SpringBoot\\src\\main\\resources\\importantPath.properties"));
-        } catch (Exception e)
-        {
+        try (InputStream inputStream = MyServletContextListener.class.getClassLoader().getResourceAsStream("importantPath.properties")){
+            // 通过类加载器获取资源文件流
+            if (inputStream == null) {
+                System.out.println("Unable to find file");
+                return;
+            }
+            // 读取属性文件
+            properties.load(inputStream);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        /* This method is called when the servlet context is initialized(when the Web application is deployed). */
-//        sce.getServletContext().setAttribute("absoluteFilePath", "/Users/liyinuo/IdeaProjects/HebeuOJ-SpringBoot/src/main/webapp/problems");
-//        sce.getServletContext().setAttribute("absoluteJudgementPath", "/Users/liyinuo/IdeaProjects/HebeuOJ-SpringBoot/src/main/webapp/judgements");
-//        sce.getServletContext().setAttribute("absoluteJudgeJavaPath", "/Users/liyinuo/IdeaProjects/HebeuOJ-SpringBoot/src/main/webapp/judgements");
-//        sce.getServletContext().setAttribute("absoluteJudgeCPath", "/Users/liyinuo/IdeaProjects/HebeuOJ-SpringBoot/src/main/webapp/judgements");
-//        sce.getServletContext().setAttribute("absoluteJudgeCPPPath", "/Users/liyinuo/IdeaProjects/HebeuOJ-SpringBoot/src/main/webapp/judgements");
-//        sce.getServletContext().setAttribute("absoluteConfigFilePath", "/Users/liyinuo/IdeaProjects/HebeuOJ-SpringBoot/src/main/webapp/judgements");
 
-        sce.getServletContext().setAttribute("absoluteIcoPath", properties.get("absoluteIcoPath"));
-        sce.getServletContext().setAttribute("absoluteFilePath", properties.get("absoluteFilePath"));
-        sce.getServletContext().setAttribute("absoluteJudgementPath", properties.get("absoluteJudgementPath"));
-        sce.getServletContext().setAttribute("absoluteJudgeMachinePath", properties.get("absoluteJudgeMachinePath"));
-        sce.getServletContext().setAttribute("absoluteCompileMachinePath", properties.get("absoluteCompileMachinePath"));
-        sce.getServletContext().setAttribute("absoluteConfigFilePath", properties.get("absoluteConfigFilePath"));
+        // 假设 properties 是已经加载好的 Properties 对象
+        for (String key : properties.stringPropertyNames()) {
+            sce.getServletContext().setAttribute(key, properties.get(key));
+        }
     }
 
     @Override
